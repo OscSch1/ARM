38a1c4c6fadc493246872b0268659dbf9a28d2ee
OscSch1 <97oscsch@gafe.molndal.se>
2023-03-24 17:26:00 +0100

    12345

--- AppServer.sh
+++ AppServer.sh
@@ -20,7 +20,7 @@ EOF
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.303.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.303.0/actions-runner-linux-x64-2.303.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.303.0.tar.gz
-./config.sh --url https://github.com/OscSch1/CICD2 --token $token
+./config.sh --url https://github.com/OscSch1/CICD2 --token $token --unattended
 systemctl daemon-reload
 systemctl enable ReverseProxy.service
 systemctl start ReverseProxy.service
